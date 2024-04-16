2fa6419833140700a8b8ced50c605b2c55e2d344
Zlybo <81806613+Zlybo@users.noreply.github.com>
2024-04-15 23:11:55 -0500

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,43 @@
+#include "Memory.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename T>
+uintptr_t getFinalAddress(const Memory& memory, uintptr_t baseAddress, const vector<uintptr_t>& offsets) {
+    uintptr_t finalAddress = baseAddress;
+    for (const auto& offset : offsets) {
+        finalAddress = memory.Read<uintptr_t>(finalAddress + offset);
+    }
+    return finalAddress;
+}
+
+int main() {
+    cout << "--------------------------------------------------" << endl;
+    cout << "Buscando y abriendo proceso de nombre Sunkenland.exe" << endl;
+    // Abro el proceso csgo.exe para acceder a sus recursos
+    const auto memory = Memory{ L"Sunkenland.exe" };
+    if (memory.isProcessOpened()) {
+        cout << "Proceso encontrado!" << endl;
+        while (1) {
+
+            const uintptr_t monoBase = memory.GetModuleAddress(L"mono-2.0-bdwgc.dll");
+            const uintptr_t addressToRead = memory.Read<uintptr_t>(monoBase + 0x007160F0);
+
+            vector<uintptr_t> offsets = { 0x70, 0x220, 0x10,0x180,0x80,0x30 };
+            const uintptr_t finalAddress = getFinalAddress<uintptr_t>(memory, addressToRead, offsets);
+
+            const int32_t metal = memory.Read<int32_t>(finalAddress + 0xAC);
+            cout << metal << endl;
+
+            if (metal != 0) {
+                memory.Write(finalAddress + 0xAC, 15);
+            }
+
+            Sleep(500);
+        }
+    }
+    else {
+        cout << "No se encontró el proceso de nombre csgo.exe" << endl;
+    }
+}
